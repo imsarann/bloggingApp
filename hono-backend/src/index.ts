ea7b9daee5c7e8cb1b0d90074c193e69a9fc7267
imsarann <shitisaesthetic@gmail.com>
2025-02-19 20:58:08 +0530

    added signup and signin routes

--- hono-backend/src/index.ts
+++ hono-backend/src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono'
 import { nextRouter } from './routes'
 
-const app = new Hono()
-// postgresql://neondb_owner:npg_JfWZ1OQNdmv3@ep-raspy-credit-a80zme5b-pooler.eastus2.azure.neon.tech/neondb?sslmode=require
+const app = new Hono<{
+    Bindings : {
+        DATABASE_URL : string,
+    }
+  }>()
 app.route('api/v1/', nextRouter);
 
 
--- hono-backend/src/routes/index.ts
+++ hono-backend/src/routes/index.ts
@@ -1,27 +1,72 @@
-// import { Router } from "hono/router"
-// export const router = Router();
-
+import { Prisma, PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { decode, sign, verify } from 'hono/jwt'
 
-export const nextRouter = new Hono();
+const privateKey = "Sivaji vaai la jilebi"
+export const nextRouter = new Hono<{
+       Bindings : {
+        DATABASE_URL : string,
+        JWT_SECRET : string
+       } 
+      }>();
 
-nextRouter.post('/user/signup/', (c) => {
-  return c.text('get user bro')
-})
+nextRouter.post("/user/signup/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-nextRouter.post("/user/signin/", (c) =>{
-  return c.text('gw');
-})
-nextRouter.post("/blog", (c)=>{
-  return c.text("asdf")
-})
-nextRouter.put("api/v1/blog", (c=>{
-    return c.text("");
-}))
-nextRouter.get('/blog/:id', (c) => {
-  return c.text('Hello Hono!')
-})
-nextRouter.get('/blog/bluk', (c) => {
-  return c.text('Hello Hono!')
-})
 
+  const body = await c.req.json();
+  await prisma.user.create({
+    data : {
+      email : body.email,
+      password : body.password,
+    }
+  })
+  // @ts-ignore
+  const token = sign({ id : user.id }, c.env.JWT_SECRET);
+
+  return c.json({
+    jwt : token
+  });
+});
+
+nextRouter.post("/user/signin/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique(
+    {
+      where : {
+        email : body.email,
+        password : body.password
+      }
+    }
+    );
+  if(!user){
+    return c.json({
+   message : "User not found"   
+  })
+  }
+  const token = await sign({ id : user.id }, c.env.JWT_SECRET);
+  return c.json({
+    token 
+  })
+});
+
+
+nextRouter.post("/blog", (c) => {
+  return c.text("asdf");
+});
+nextRouter.put("api/v1/blog", (c) => {
+  return c.text("");
+});
+nextRouter.get("/blog/:id", (c) => {
+  return c.text("Hello Hono!");
+});
+nextRouter.get("/blog/bluk", (c) => {
+  return c.text("Hello Hono!");
+});
